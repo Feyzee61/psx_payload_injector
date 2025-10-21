8a1413aac18e225886a8295fd150c1462e72f558
Fevzi Zengin <fevzizengin@gmail.com>
2025-10-22 00:31:10 +0300

    Update Form1.Designer.cs for UI enhancements

--- PayloadInjector/Form1.Designer.cs
+++ PayloadInjector/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PayloadInjector
+namespace PayloadInjector
 {
     partial class Form1
     {
@@ -30,11 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbElfSize = new System.Windows.Forms.Label();
             this.tbElfFile = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tbElfPort = new System.Windows.Forms.TextBox();
             this.btnSendElf = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbBinSize = new System.Windows.Forms.Label();
             this.tbBinFile = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.tbBinPort = new System.Windows.Forms.TextBox();
@@ -43,13 +45,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnSendPs4ElfLdr = new System.Windows.Forms.Button();
             this.btnSendPs5ElfLdr = new System.Windows.Forms.Button();
-            this.lbBinSize = new System.Windows.Forms.Label();
-            this.lbElfSize = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.gbPayloading = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.gbPayloading.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -61,12 +64,22 @@
             this.groupBox1.Controls.Add(this.btnSendElf);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(12, 253);
+            this.groupBox1.Location = new System.Drawing.Point(12, 269);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(336, 111);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ELF Injector";
+            // 
+            // lbElfSize
+            // 
+            this.lbElfSize.AutoSize = true;
+            this.lbElfSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbElfSize.Location = new System.Drawing.Point(6, 47);
+            this.lbElfSize.Name = "lbElfSize";
+            this.lbElfSize.Size = new System.Drawing.Size(142, 16);
+            this.lbElfSize.TabIndex = 10;
+            this.lbElfSize.Text = "Load an elf file to inject";
             // 
             // tbElfFile
             // 
@@ -120,12 +133,22 @@
             this.groupBox2.Controls.Add(this.btnSendBin);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox2.ForeColor = System.Drawing.Color.White;
-            this.groupBox2.Location = new System.Drawing.Point(12, 136);
+            this.groupBox2.Location = new System.Drawing.Point(12, 152);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(336, 111);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "BIN Injector";
+            // 
+            // lbBinSize
+            // 
+            this.lbBinSize.AutoSize = true;
+            this.lbBinSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbBinSize.Location = new System.Drawing.Point(6, 47);
+            this.lbBinSize.Name = "lbBinSize";
+            this.lbBinSize.Size = new System.Drawing.Size(139, 16);
+            this.lbBinSize.TabIndex = 8;
+            this.lbBinSize.Text = "Load a bin file to inject";
             // 
             // tbBinFile
             // 
@@ -199,11 +222,11 @@
             this.btnSendPs4ElfLdr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSendPs4ElfLdr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnSendPs4ElfLdr.ForeColor = System.Drawing.Color.White;
-            this.btnSendPs4ElfLdr.Location = new System.Drawing.Point(6, 55);
+            this.btnSendPs4ElfLdr.Location = new System.Drawing.Point(6, 58);
             this.btnSendPs4ElfLdr.Name = "btnSendPs4ElfLdr";
-            this.btnSendPs4ElfLdr.Size = new System.Drawing.Size(150, 32);
+            this.btnSendPs4ElfLdr.Size = new System.Drawing.Size(150, 45);
             this.btnSendPs4ElfLdr.TabIndex = 1;
-            this.btnSendPs4ElfLdr.Text = "Inject PS4 Elf Loader";
+            this.btnSendPs4ElfLdr.Text = "Inject PS4 Elf Loader to Port 9090";
             this.btnSendPs4ElfLdr.UseVisualStyleBackColor = true;
             this.btnSendPs4ElfLdr.Click += new System.EventHandler(this.btnSendPs4ElfLdr_Click);
             // 
@@ -212,33 +235,13 @@
             this.btnSendPs5ElfLdr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSendPs5ElfLdr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnSendPs5ElfLdr.ForeColor = System.Drawing.Color.White;
-            this.btnSendPs5ElfLdr.Location = new System.Drawing.Point(180, 55);
+            this.btnSendPs5ElfLdr.Location = new System.Drawing.Point(180, 58);
             this.btnSendPs5ElfLdr.Name = "btnSendPs5ElfLdr";
-            this.btnSendPs5ElfLdr.Size = new System.Drawing.Size(150, 32);
+            this.btnSendPs5ElfLdr.Size = new System.Drawing.Size(150, 45);
             this.btnSendPs5ElfLdr.TabIndex = 2;
-            this.btnSendPs5ElfLdr.Text = "Inject PS5 Elf Loader";
+            this.btnSendPs5ElfLdr.Text = "Inject PS5 Elf Loader to Port 9020";
             this.btnSendPs5ElfLdr.UseVisualStyleBackColor = true;
             this.btnSendPs5ElfLdr.Click += new System.EventHandler(this.btnSendPs5ElfLdr_Click);
-            // 
-            // lbBinSize
-            // 
-            this.lbBinSize.AutoSize = true;
-            this.lbBinSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbBinSize.Location = new System.Drawing.Point(6, 47);
-            this.lbBinSize.Name = "lbBinSize";
-            this.lbBinSize.Size = new System.Drawing.Size(139, 16);
-            this.lbBinSize.TabIndex = 8;
-            this.lbBinSize.Text = "Load a bin file to inject";
-            // 
-            // lbElfSize
-            // 
-            this.lbElfSize.AutoSize = true;
-            this.lbElfSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbElfSize.Location = new System.Drawing.Point(6, 47);
-            this.lbElfSize.Name = "lbElfSize";
-            this.lbElfSize.Size = new System.Drawing.Size(142, 16);
-            this.lbElfSize.TabIndex = 10;
-            this.lbElfSize.Text = "Load an elf file to inject";
             // 
             // groupBox3
             // 
@@ -249,7 +252,7 @@
             this.groupBox3.ForeColor = System.Drawing.Color.White;
             this.groupBox3.Location = new System.Drawing.Point(12, 37);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(336, 93);
+            this.groupBox3.Size = new System.Drawing.Size(336, 109);
             this.groupBox3.TabIndex = 6;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Elf Loader";
@@ -258,11 +261,31 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(25, 27);
+            this.label4.Location = new System.Drawing.Point(24, 27);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(288, 16);
             this.label4.TabIndex = 9;
             this.label4.Text = "Inject Elf Loader payload to support Elf Injection";
+            // 
+            // gbPayloading
+            // 
+            this.gbPayloading.Controls.Add(this.label5);
+            this.gbPayloading.Location = new System.Drawing.Point(12, 135);
+            this.gbPayloading.Name = "gbPayloading";
+            this.gbPayloading.Size = new System.Drawing.Size(336, 139);
+            this.gbPayloading.TabIndex = 7;
+            this.gbPayloading.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(63, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sending Payload, please wait ... ...";
             // 
             // Form1
             // 
@@ -270,19 +293,20 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
-            this.ClientSize = new System.Drawing.Size(360, 376);
+            this.ClientSize = new System.Drawing.Size(360, 393);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbIPAddress);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbPayloading);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Payload Injector v1.1 by Feyzee";
+            this.Text = "Payload Injector v1.3 by Feyzee";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
@@ -293,6 +317,8 @@
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.gbPayloading.ResumeLayout(false);
+            this.gbPayloading.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -318,7 +344,8 @@
         private System.Windows.Forms.Label lbBinSize;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox gbPayloading;
+        private System.Windows.Forms.Label label5;
     }
 }
-
 
